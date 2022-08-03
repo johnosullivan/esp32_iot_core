814b26420cb90d682050738ce0387025f1ef0afd
John O'Sullivan <jnosullivan@icloud.com>
2022-08-02 21:42:14 -0500

    updated core

--- s3/main/app_main.c
+++ s3/main/app_main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <assert.h>
 
 #include "sdkconfig.h"
 
@@ -18,199 +20,49 @@
 #include "freertos/queue.h"
 
 #include "utils.h"
+#include "iridium.h"
 #include "bt_manager.h"
 #include "wifi_manager.h"
 
+#include <pthread.h>
+
 static const char *TAG_CORE = "esp32_hub_core";
 
-//#define ECHO_TEST_RTS (UART_PIN_NO_CHANGE)
-//#define ECHO_TEST_CTS (UART_PIN_NO_CHANGE)
-//static const int RX_BUF_SIZE = 1024;
-
-#define TXD_PIN (GPIO_NUM_17)
-#define RXD_PIN (GPIO_NUM_18)
-
-#define UART UART_NUM_1
-
-int num = 0;
-
-#define BUF_SIZE (4096)
-#define RD_BUF_SIZE (BUF_SIZE)
-
-//static QueueHandle_t uart0_queue;
-
-typedef struct stack {
-    int cap;
-    int size;
-    char** item;
-} Stack;
-
-Stack* creatstack(int maxSize) {
-    Stack* s;
-    s=(Stack*)malloc(sizeof(Stack));
-    s->item=(char**)malloc(sizeof(char)*5); 
-    s->cap=maxSize;
-    s->size=0;
-    return s;
-}
-
-void push(Stack* s, char* data)
-{
-    s->item[s->size++]=data;
-}
- 
-char* top(Stack* s)
-{
-    return s->item[s->size-1];
-}
- 
-void pop(Stack* s)
-{
-    s->size--;
-}
-
-
-typedef struct {
-    QueueHandle_t uart_queue;
-} iridium_settings_t;
-
-//extern struct iridium_settings_t satcom_settings;
-
-
-void uart_event_task0(void *pvParameters) {
-    iridium_settings_t satcom = *(iridium_settings_t *)pvParameters;
-
-    uart_event_t event;
-    //size_t buffered_size;
-    uint8_t* dtmp = (uint8_t*) malloc(RD_BUF_SIZE);
-
-    Stack *s = creatstack(10);
-
+void system_monitoring_task(void *pvParameter) {
+    ESP_LOGI(TAG_CORE, "System [system_monitoring_task]");
     for(;;) {
-        //Waiting for UART event.
-        if(xQueueReceive(satcom.uart_queue, (void * )&event, (portTickType)portMAX_DELAY)) {
-            bzero(dtmp, RD_BUF_SIZE);
-            ESP_LOGI(TAG_CORE, "uart[%d] event:", UART);
-            switch(event.type) {
-                //Event of UART receving data
-                /*We'd better handler data event fast, there would be much more data events than
-                other types of events. If we take too much time on data event, the queue might
-                be full.*/
-                case UART_DATA:
-                    uart_read_bytes(UART, dtmp, event.size, portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(30000));
+    }
+}
 
-                    ESP_LOGI(TAG_CORE, "\n%s", dtmp);
-
-                    char* pch = NULL;
-                    pch = strtok((char*)dtmp, "\r\n");
-
-                    while (pch != NULL)
-                    {
-                        printf("- %s\n", pch);
-                        pch = strtok(NULL, "\r\n");
-                    }
-
-                    break;
-                //Event of HW FIFO overflow detected
-                case UART_FIFO_OVF:
-                    // If fifo overflow happened, you should consider adding flow control for your application.
-                    // The ISR has already reset the rx FIFO,
-                    // As an example, we directly flush the rx buffer here in order to read more data.
-                    uart_flush_input(UART);
-                    xQueueReset(satcom.uart_queue);
-                    break;
-                //Event of UART ring buffer full
-                case UART_BUFFER_FULL:
-                    // If buffer full happened, you should consider encreasing your buffer size
-                    // As an example, we directly flush the rx buffer here in order to read more data.
-                    uart_flush_input(UART);
-                    xQueueReset(satcom.uart_queue);
-                    break;
-                //Event of UART RX break detected
-                case UART_BREAK:
-                    ESP_LOGI(TAG_CORE, "UART_BREAK");
-                    break;
-                //Event of UART parity check error
-                case UART_PARITY_ERR:
-                    break;
-                //Event of UART frame error
-                case UART_FRAME_ERR:
-                    break;
-                //UART_PATTERN_DET
-                case UART_PATTERN_DET:
-                    break;
-                //Others
-                default:
-                    break;
-            }
+void cb_satcom(iridium_t* satcom, iridium_command_t command, iridium_status_t status) { 
+    if (status == SAT_OK) {
+        switch (command) {
+            case AT_CSQ:
+                ESP_LOGI(TAG_CORE, "Signal Strength [0-5]: %d", satcom->signal_strength);
+                break;
+            case AT_CGMM:
+                ESP_LOGI(TAG_CORE, "Model Identification: %s", satcom->model_identification);
+                break;
+            case AT_CGMI:
+                ESP_LOGI(TAG_CORE, "Manufacturer Identification: %s", satcom->manufacturer_identification);
+                break;
+            default:
+                break;
         }
     }
-    free(dtmp);
-    dtmp = NULL;
-    vTaskDelete(NULL);
 }
 
+void cb_message(iridium_t* satcom, char* data) { 
+    ESP_LOGI(TAG_CORE, "CALLBACK[INCOMING] %s", data);
 
-void iridium_config(/*iridium_settings_t *stacom*/) {
-    iridium_settings_t stacom;
-
-    uart_config_t uart_config = {
-        .baud_rate = 19200,
-        .data_bits = UART_DATA_8_BITS,
-        .parity    = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        //.source_clk = UART_SCLK_REF_TICK,
-    };
-
-    ESP_ERROR_CHECK(uart_driver_install(UART, BUF_SIZE * 2, BUF_SIZE * 2, 20, &stacom.uart_queue, 0));
-
-    ESP_ERROR_CHECK(uart_param_config(UART, &uart_config));
-
-    ESP_ERROR_CHECK(uart_set_pin(UART, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
-
-    xTaskCreatePinnedToCore(&uart_event_task0, "uart_event_task0", 2048, &stacom, 12, NULL, 1);
-}
-
-
-
-void system_monitoring_task(void *pvParameter) {
-    vTaskDelay(pdMS_TO_TICKS(10000));
-    /* Configure parameters of an UART driver,
-     * communication pins and install the driver */
-    /*uart_config_t uart_config = {
-        .baud_rate = 19200,
-        .data_bits = UART_DATA_8_BITS,
-        .parity    = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .source_clk = UART_SCLK_REF_TICK,
-    };
-    int intr_alloc_flags = 0;
-
-    ESP_ERROR_CHECK(uart_driver_install(UART, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
-    ESP_ERROR_CHECK(uart_param_config(UART, &uart_config));
-    ESP_ERROR_CHECK(uart_set_pin(UART, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));*/
-
-    // Configure a temporary buffer for the incoming data
-    //uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
-    char* data = (char*) malloc(100);
-
-    for(;;) {
-          vTaskDelay(pdMS_TO_TICKS(30000));
-          // PING SATCOM AT+ COMMAND
-          data = "AT+CSQ\r";
-          //ESP_LOGI(TAG_CORE, "uart_write_bytes");
-          uart_write_bytes(UART, data, strlen(data));
-
-          //vTaskDelay(5000 / portTICK_PERIOD_MS);
-          /*uint8_t* dd = (uint8_t*) malloc(BUF_SIZE+1);
-          const int rxBytes = uart_read_bytes(UART, dd, BUF_SIZE, 4000 / portTICK_RATE_MS);
-          ESP_LOGI(TAG_CORE, "uart_read_bytes");
-          ESP_LOGI(TAG_CORE, "%d bytes", rxBytes);
-          ESP_LOGI(TAG_CORE, "%s", dd);
-
-          ESP_ERROR_CHECK(uart_flush(UART_NUM_1));*/
+    if (strcmp ("PING", data) == 0) {
+        iridium_result_t r1 = iridium_tx_message(satcom, "PONG");
+        if (r1.status != SAT_OK) {
+            ESP_LOGI(TAG_CORE, "R[%d] TX Failed!", r1.status);
+        } else {
+            ESP_LOGI(TAG_CORE, "R[%d] = %s", r1.status, r1.result);
+        }
     }
 }
 
@@ -251,7 +103,7 @@ void app_main(void)
         err = nvs_get_i32(handle, MIHOME_STORAGE_IS_CONFIG, &configured);
         switch (err) {
             case ESP_OK:
-                ESP_LOGD(TAG_CORE, "MIHOME_STORAGE_IS_CONFIG = %d\n", configured);
+                ESP_LOGD(TAG_CORE, "STORAGE_IS_CONFIG = %d\n", configured);
                 if (configured) {
                     /* Start WiFi Manager + Callback (EVENT_STA_GOT_IP) */
                     wifi_manager_start();
@@ -283,15 +135,38 @@ void app_main(void)
         esp_restart();
     }
 
-    /* Config Iridium SatCom via UART */
-    iridium_config();
 
+
+    /* Configuration Iridium SatCom */
+    iridium_t *satcom = iridium_default_configuration();
+    satcom->callback = &cb_satcom;
+    satcom->message_callback = &cb_message;
+    /* UART Port Configuration */
+    satcom->uart_number = UART_NUM_1;
+    satcom->uart_txn_number = GPIO_NUM_17;
+    satcom->uart_rxd_number = GPIO_NUM_18;
+    satcom->uart_rts_number = UART_PIN_NO_CHANGE;
+    satcom->uart_cts_number = UART_PIN_NO_CHANGE;
+    
+    /* Initialized */
+    if (iridium_config(satcom) == SAT_OK) {
+        ESP_LOGI(TAG_CORE, "Iridium Modem [Initialized]");
+    }
+
+    /* Allow Ring Triggers */
+    iridium_result_t ring = iridium_config_ring(satcom, true);
+    if (ring.status == SAT_OK) {
+        ESP_LOGI(TAG_CORE, "Iridium Modem [Ring Enabled]");
+    }
+   
     /* Create FreeRTOS Monitoring Task */
     xTaskCreatePinnedToCore(&system_monitoring_task, "system_monitoring_task", 2048, NULL, 1, NULL, 1);
 
-    vTaskDelay(pdMS_TO_TICKS(10000));
-
-    char* data = (char*) malloc(100);
-    data = "AT+SBDSX\r";
-    uart_write_bytes(UART, data, strlen(data));
+    for(;;) {
+        iridium_result_t r1 = iridium_send(satcom, AT_CSQ, "", true, 500);
+        if (r1.status == SAT_OK) {
+            ESP_LOGI(TAG_CORE, "R[%d] = %s", r1.status, r1.result);
+        }                         
+        vTaskDelay(pdMS_TO_TICKS(120000));
+    }
 }
---
+++ s3/main/iridium.h
@@ -0,0 +1,271 @@
+#ifndef IRIDIUM_H_INCLUDED
+#define IRIDIUM_H_INCLUDED
+
+/**
+ * @author John O'Sullivan
+ * @paragraph ESP32 Satcom Library (Iridium Network)
+ * Language:  C/C++
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <pthread.h>
+
+#include "nvs.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_log.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+#include "freertos/queue.h"
+
+#include "stack.h"
+
+#define IRI_BUF_SIZE    (4096)
+#define IRI_RD_BUF_SIZE (IRI_BUF_SIZE)
+#define IRI_BUFF_DELAY  (100)
+
+/**
+ * @brief the enum to represent the AT commands. 
+ */
+typedef enum iridium_command {
+    SBDRING         = -1,
+    AT              = 0,
+    AT_CSQ          = 1,
+    AT_SBDSX        = 2,
+    AT_CGMI         = 3,
+    AT_CGMM         = 4,
+    AT_SBDRT        = 5,
+    AT_SBDWT        = 6,
+    AT_SBDIX        = 7,
+    AT_MSSTM        = 8,
+    AT_SBDMTA       = 9, 
+    AT_W0           = 10,  
+    AT_CRIS         = 11,    
+    AT_SBDIXA       = 12,
+    AT_K0           = 13,
+    AT_SBDMTAQ      = 14,
+
+} iridium_command_t;
+
+/**
+ * @brief the iridium command status.
+ */
+typedef enum iridium_status {
+    SAT_ERROR       = -1,
+    SAT_OK          = 1 
+} iridium_status_t;
+
+/**
+ * @brief the iridium UART queue status.
+ */
+typedef enum iridium_queue_status {
+    IQS_NONE        = -1,
+    IQS_OPEN        = 0,
+    IQS_WAITING     = 1 
+} iridium_queue_status_t;
+
+typedef enum iridium_mt_status {
+    /* <MT status> */
+    MT_NO_SBD_MESSAGE_RECEIVED              = 0, // No SBD message to receive from the GSS.
+    MT_SBD_MESSAGE_SUCCESSFULLY_RECEIVED    = 1, // SBD message successfully received from the GSS.
+    MT_GSS_ERROR_OCCURRED                   = 2  // An error occurred while attempting to perform a mailbox check or receive a message from the GSS.
+} iridium_mt_status_t;
+
+typedef enum iridium_mo_status {
+    /* <MO status> */
+    MO_TRANSFERRED_SUCCESSFULLY                     = 0,  // MO message, if any, transferred successfully.
+    MO_TRANSFERRED_SUCCESSFULLY_TOO_BIG             = 1,  // MO message, if any, transferred successfully, but the MT message in the queue was too big to be transferred.
+    MO_TRANSFERRED_SUCCESSFULLY_LOC_NOT_ACCEPTED    = 2,  // MO message, if any, transferred successfully, but the requested Location Update was not accepted.
+    MO_GSS_NOT_COMPLETED                            = 10, // GSS reported that the call did not complete in the allowed time.
+    MO_GSS_MESSAGE_QUEUE_FULL                       = 11, // MO message queue at the GSS is full.
+    MO_GSS_MESSAGE_MANY_SEQ                         = 12, // MO message has too many segments.
+    MO_GSS_MESSAGE_SESSION_INCOMPLETE               = 13, // GSS reported that the session did not complete.
+    MO_INVALID_SEQMENT_SIZE                         = 14, // Invalid segment size.
+    MO_ACCESS_DENIED                                = 15, // Access is denied.
+    MO_ISU_LOCKED                                   = 16, // ISU has been locked and may not make SBD calls (see +CULK command).
+    MO_GATEWAY_NOT_RESPONDING                       = 17, // Gateway not responding (local session timeout).
+    MO_CONNECTION_LOST                              = 18, // Connection lost (RF drop).
+    MO_LINK_FAILURE                                 = 19, // Link failure (A protocol error caused termination of the call).
+    MO_NO_NETWORK_SERVICE                           = 32, // No network service, unable to initiate call.
+    MO_ANTENNA_FAULT                                = 33, // Antenna fault, unable to initiate call.
+    MO_RADIO_DISABLED                               = 34, // Radio is disabled, unable to initiate call (see *Rn command).
+    MO_ISU_IS_BUSY                                  = 35, // ISU is busy, unable to initiate call.
+    MO_TRY_LATER_3_MIN                              = 36, // Try later, must wait 3 minutes since last registration.
+    MO_SBD_SERVICE_TERMP_DISABLED                   = 37, // SBD service is temporarily disabled.
+    MO_TRY_LATER_TRAFFIC_PERIOD                     = 38, // Try later, traffic management period (see +SBDLOE command)
+    MO_BAND_VIOLATION                               = 64, // Band violation (attempt to transmit outside permitted frequency band).
+    MO_PLL_LOCK_FAILURE                             = 65  // PLL lock failure; hardware error during attempted transmit.
+} iridium_mo_status_t;
+
+/**
+ * @brief the core iridum struct with all configuration / status values.
+ * 
+ * @paragraph 
+ * 
+ * 0 - 4 = Transmit successful
+ * 32    = No network service
+ * MO    = Mobile Originated 
+ * MT    = Mobile Terminated
+ */
+typedef struct iridium {
+    QueueHandle_t uart_queue;
+    QueueHandle_t buffer_queue;
+    QueueHandle_t message_queue;
+    /* signal */
+    int signal_strength;
+    /* messaging */
+    int status_inbound;
+    int status_outbound;
+    int sequence_inbound;
+    int sequence_outbound;
+    int bytes_received;
+    int messages_waiting;
+    /* about */
+    char manufacturer_identification[20];
+    char model_identification[50];
+    /* quene processing */
+    int c_nonce;
+    int p_nonce;
+    int buffer_size;
+    int buffer_delay_ms;
+    int uart_number;
+    int uart_txn_number;
+    int uart_rxd_number;
+    int uart_rts_number;
+    int uart_cts_number;
+    int ring_task_running;
+    char buffer_data[100];
+    iridium_queue_status_t status;
+    pthread_mutex_t p_status_mutex;
+    pthread_mutex_t p_nonce_mutex;
+    /* stack sizes */
+    int task_message_stack_depth;
+    int task_buffer_stack_depth;
+    int task_uart_stack_depth;
+    /* callbacks */ 
+    void (*callback) (struct iridium* satcom, iridium_command_t command, iridium_status_t status);
+    void (*message_callback) (struct iridium* satcom, char* data);
+} iridium_t;
+
+/**
+ * @brief the iridium message for the modem.
+ */
+typedef struct iridium_message {
+  char data[50];
+  int size;
+  int nonce;
+  int command;
+} iridium_message_t;
+
+/**
+ * @brief the iridium result from the modem.
+ */
+typedef struct iridium_result {
+    char result[50];
+    iridium_status_t status;
+} iridium_result_t;
+
+/**
+ * @brief callbacks required for message/event data.
+ */
+typedef void (*callback_t) (iridium_t* satcom, iridium_command_t command, iridium_status_t status);
+typedef void (*message_callback_t) (iridium_t* satcom, char* data);
+
+/**
+ * @brief Process data returned to device from UART bus. 
+ * @param satcom the iridium_t struct pointer.
+ * @param command the AT command being processed.
+ * @param data the data returned to be parsed into iridium_t struct.
+ * @return a iridium_status_t with SAT_OK or SAT_ERROR value.
+ */
+iridium_status_t iridium_satcom_process_result(iridium_t *satcom, char *command, char *data);
+
+/**
+ * @brief Update the iridium queue status. 
+ * @param satcom the iridium_t struct pointer.
+ * @param status the current IQS status.
+ * @return a iridium_status_t with SAT_OK or SAT_ERROR value.
+ */
+iridium_status_t iridium_update_iqs(iridium_t* satcom, iridium_queue_status_t status);
+
+/**
+ * @brief Update the processing message nonce. 
+ * @param satcom the iridium_t struct pointer.
+ * @param nonce the message nonce int.
+ * @return a iridium_status_t with SAT_OK or SAT_ERROR value.
+ */
+iridium_status_t iridium_update_p_nonce(iridium_t* satcom, int nonce);
+
+/**
+ * @brief Retrieves the current queue status while processing a message nonce. 
+ * @param satcom the iridium_t struct pointer.
+ * @return a iridium_queue_status_t with IQS_NONE, IQS_OPEN or IQS_WAITING value.
+ */
+iridium_queue_status_t iridium_get_iqs(iridium_t* satcom); 
+
+/**
+ * @brief Send data payload across the UART bus.
+ * @param satcom the iridium_t struct pointer.
+ * @param data the data to be sent. 
+ * @param nonce the nonce used to track responses. 
+ * @return a iridium_status_t with SAT_OK or SAT_ERROR value.
+ */
+iridium_status_t iridium_send_raw(iridium_t* satcom, char *data, int nonce);
+
+/**
+ * @brief Send AT command with data.  
+ * @param satcom the iridium_t struct pointer.
+ * @param command the iridium modem AT command.
+ * @param rdata the raw data. 
+ * @param wait_response wait for a responce from the modem.
+ * @param wait_interval the amount of time in ms for wait interval check.
+ * @return a iridium_result_t with metadata.
+ */
+iridium_result_t iridium_send(iridium_t* satcom, iridium_command_t command, char *rdata, bool wait_response, int wait_interval);
+
+/**
+ * @brief Configure iridium modem via UART connection. 
+ * @param satcom the iridium_t struct pointer.
+ * @return a iridium_status_t with SAT_OK or SAT_ERROR value.
+ */
+iridium_status_t iridium_config(iridium_t *satcom);
+
+/**
+ * @brief Enabled or disable the ring notification on the modem.  
+ * @param satcom the iridium_t struct pointer.
+ * @param enabled the ring notification.
+ * @return a iridium_result_t with metadata.
+ */
+iridium_result_t iridium_config_ring(iridium_t *satcom, bool enabled);
+
+/**
+ * @brief Transmit a message to the iridium network.
+ * @param satcom the iridium_t struct pointer.
+ * @param message to be sent.
+ * @return a iridium_result_t with metadata.
+ */
+iridium_result_t iridium_tx_message(iridium_t *satcom, char *message);
+
+/**
+ * @brief Create a default iridium configuration.
+ * @return a valid iridium_t struct configuration.
+ */
+iridium_t* iridium_default_configuration();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* IRIDIUM_H_INCLUDED */
